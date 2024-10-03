840dac6b749a8e96a4b96244d2ed945c26c0162b
Ekaterina Kotliarenko <kagerka@gmail.com>
2024-10-03 10:15:30 +0300

    feat: add solution to copy function

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,15 @@
+import { access, constants, cp } from 'fs';
+
 const copy = async () => {
-    // Write your code here 
+    access('./src/fs/files_copy', constants.F_OK, (notExist) => {
+        if (notExist) {
+            cp('./src/fs/files', './src/fs/files_copy', { recursive: true }, (err) => {
+                if (err) throw new Error('FS operation failed');
+            });
+        } else {
+            throw new Error('FS operation failed');
+        }
+    })
 };
 
 await copy();
