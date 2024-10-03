c5f5c65e3902d3cfd8bd54116a9f1913d6288620
Ekaterina Kotliarenko <kagerka@gmail.com>
2024-10-03 12:39:31 +0300

    feat: add solution to list function

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,13 @@
+import fs from "fs";
+
 const list = async () => {
-    // Write your code here 
+  fs.readdir("./src/fs/files", (err, files) => {
+    if (err) {
+      throw new Error("FS operation failed");
+    } else {
+      console.log(files);
+    }
+  });
 };
 
 await list();
