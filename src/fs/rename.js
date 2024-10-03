df227490f74e588a3131749006159245b9e79d84
Ekaterina Kotliarenko <kagerka@gmail.com>
2024-10-03 10:40:22 +0300

    feat: add solution to rename function

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,15 @@
+import fs from "fs";
+
 const rename = async () => {
-    // Write your code here 
+  fs.access("./src/fs/files/properFilename.md", fs.constants.F_OK, (notExist) => {
+    if (notExist) {
+      fs.rename("./src/fs/files/wrongFilename.txt", "./src/fs/files/properFilename.md", (err) => {
+        if (err) throw new Error("FS operation failed");
+      });
+    } else {
+      throw new Error("FS operation failed");
+    }
+  });
 };
 
 await rename();
