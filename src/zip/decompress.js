ef68ac4f126d84ef6763882aa2f425594bbef1ce
Ekaterina Kotliarenko <kagerka@gmail.com>
2024-10-07 21:01:57 +0300

    feat: add solution to decompress function

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,10 @@
+import fs from "fs";
+import zlib from "zlib";
+
 const decompress = async () => {
-    // Write your code here 
+  fs.createReadStream("./src/zip/files/archive.gz")
+    .pipe(zlib.createGunzip())
+    .pipe(fs.createWriteStream("./src/zip/files/fileToCompress.txt"));
 };
 
 await decompress();
