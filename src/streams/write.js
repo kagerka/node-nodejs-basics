222b173f8e619a21e3ec06bdb9537aa330e2f6fc
Ekaterina Kotliarenko <kagerka@gmail.com>
2024-10-07 12:49:51 +0300

    feat: add solution to write function

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,7 @@
+import fs from "fs";
 const write = async () => {
-    // Write your code here 
+  const writeStream = fs.createWriteStream("./src/streams/files/fileToWrite.txt", "utf8");
+  process.stdin.pipe(writeStream);
 };
 
 await write();
