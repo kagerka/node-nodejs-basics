a0d1a6e4076beff401b38a534a697cf71b3aafaa
Ekaterina Kotliarenko <kagerka@gmail.com>
2024-10-07 11:32:28 +0300

    feat: add solution to calculateHash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,14 @@
+import fs from "fs";
 const calculateHash = async () => {
-    // Write your code here 
+  const crypto = await import("crypto");
+  const hash = crypto.createHash("sha256");
+  const readableStream = fs.createReadStream("./src/hash/files/fileToCalculateHashFor.txt");
+  readableStream
+    .on("data", (data) => {
+      hash.update(data);
+    })
+    .on("end", () => {
+      console.log(hash.digest("hex"));
+    });
 };
-
 await calculateHash();
