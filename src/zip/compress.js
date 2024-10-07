f23498a42011bf480bea365eb6abc2e5889f9201
Ekaterina Kotliarenko <kagerka@gmail.com>
2024-10-07 20:52:33 +0300

    feat: add solution to compress function

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,10 @@
+import fs from "fs";
+import zlib from "zlib";
+
 const compress = async () => {
-    // Write your code here 
+  fs.createReadStream("./src/zip/files/fileToCompress.txt")
+    .pipe(zlib.createGzip())
+    .pipe(fs.createWriteStream("./src/zip/files/archive.gz"));
 };
 
 await compress();
