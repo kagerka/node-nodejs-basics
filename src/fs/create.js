81b9a22bd509637af40187a8c7db1ce1987b3aa8
Ekaterina Kotliarenko <kagerka@gmail.com>
2025-04-25 00:07:39 +0300

    feat: add solution to create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,20 @@
+import { stat, writeFile } from "fs";
+
+const fileName = "./src/fs/files/fresh.txt";
+const fileContent = "I am fresh and young";
+
 const create = async () => {
-    // Write your code here 
+  stat(fileName, (error) => {
+    if (error) {
+      writeFile(fileName, fileContent, (error) => {
+        if (!error) {
+          console.log("File was created successfully!");
+        }
+      });
+    } else {
+      throw new Error("FS operation failed");
+    }
+  });
 };
 
 await create();
