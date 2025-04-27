f831045352a65b88eff34febca2b63ca058c6a79
Ekaterina Kotliarenko <kagerka@gmail.com>
2025-04-27 15:31:07 +0300

    feat: add solution for list.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,16 @@
+import { readdir } from "fs";
+import pathName from "path";
+
+const folderName = pathName.join(import.meta.dirname, "files");
+
 const list = async () => {
-    // Write your code here 
+  readdir(folderName, (error, data) => {
+    if (error) {
+      throw new Error("FS operation failed");
+    } else {
+      console.log(data);
+    }
+  });
 };
 
 await list();
