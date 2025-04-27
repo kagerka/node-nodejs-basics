58aa6f50851cc4385bc9645262eb3685d95af2cc
Ekaterina Kotliarenko <kagerka@gmail.com>
2025-04-27 21:27:12 +0300

    feat: add solution for calcHash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,18 @@
+import { createHash } from "node:crypto";
+import { createReadStream } from "node:fs";
+import path from "node:path";
+
 const calculateHash = async () => {
-    // Write your code here 
+  const hash = createHash("sha256");
+  const fileName = path.join(import.meta.dirname, "files", "fileToCalculateHashFor.txt");
+  const readStream = createReadStream(fileName);
+  readStream
+    .on("data", (data) => {
+      hash.update(data);
+    })
+    .on("end", () => {
+      console.log(hash.digest("hex"));
+    });
 };
 
 await calculateHash();
