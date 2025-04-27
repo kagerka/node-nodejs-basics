7638f7217907f94ce844360d9ba576fcb0e5bf00
Ekaterina Kotliarenko <kagerka@gmail.com>
2025-04-27 15:51:23 +0300

    feat: add solution for read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,16 @@
+import { readFile } from "fs";
+import pathName from "path";
+
+const fileName = pathName.join(import.meta.dirname, "files", "fileToRead.txt");
+
 const read = async () => {
-    // Write your code here 
+  readFile(fileName, "utf8", (error, data) => {
+    if (error) {
+      throw new Error("FS operation failed");
+    } else {
+      console.log(data);
+    }
+  });
 };
 
 await read();
