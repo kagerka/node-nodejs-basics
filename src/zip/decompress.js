c3ef3af2ef4fc37863fc29bc8a7c5ae211bfd2e6
Ekaterina Kotliarenko <kagerka@gmail.com>
2025-04-28 09:11:29 +0300

    feat: add solution for decompress.js

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,19 @@
+import { access, createReadStream, createWriteStream } from "node:fs";
+import path from "node:path";
+import { pipeline } from "node:stream/promises";
+import { createGunzip } from "node:zlib";
+
+const fileName = path.join(import.meta.dirname, "files", "fileToCompress.txt");
+const archiveName = path.join(import.meta.dirname, "files", "archive.gz");
+
 const decompress = async () => {
-    // Write your code here 
+  access(archiveName, (notExist) => {
+    if (notExist) {
+      throw new Error("Archive not found!");
+    } else {
+      pipeline(createReadStream(archiveName), createGunzip(), createWriteStream(fileName));
+    }
+  });
 };
 
 await decompress();
