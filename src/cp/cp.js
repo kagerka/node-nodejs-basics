b05547876d50fb746737888e427d6f8e87ea45a1
Ekaterina Kotliarenko <kagerka@gmail.com>
2025-04-29 00:21:45 +0300

    feat: add solution for cp.js

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,26 @@
+import { spawn } from "node:child_process";
+import path from "node:path";
+import { pipeline } from "node:stream";
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const fileName = path.join(import.meta.dirname, "files", "script.js");
+  const child = spawn("node", [fileName, ...args], { stdio: "pipe" });
+
+  const onError = (error) => {
+    console.error("Process failed with an error: ", error);
+    process.exit(1);
+  };
+
+  pipeline(process.stdin, child.stdin, (error) => {
+    if (error) {
+      onError(error);
+    }
+  });
+  pipeline(child.stdout, process.stdout, (error) => {
+    if (error) {
+      onError(error);
+    }
+  });
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(["someArgument1", "someArgument2", "someArgument3"]);
