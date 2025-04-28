1e69a1501d48437af235deaf049e377942b71efd
Ekaterina Kotliarenko <kagerka@gmail.com>
2025-04-28 09:20:16 +0300

    fix: add error check for compress.js

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,12 +1,19 @@
-import { createReadStream, createWriteStream } from "node:fs";
+import { access, createReadStream, createWriteStream } from "node:fs";
 import path from "node:path";
+import { pipeline } from "node:stream/promises";
 import { createGzip } from "node:zlib";
 
 const fileName = path.join(import.meta.dirname, "files", "fileToCompress.txt");
 const archiveName = path.join(import.meta.dirname, "files", "archive.gz");
 
 const compress = async () => {
-  createReadStream(fileName).pipe(createGzip()).pipe(createWriteStream(archiveName));
+  access(fileName, (notExist) => {
+    if (notExist) {
+      throw new Error("File not found!");
+    } else {
+      pipeline(createReadStream(fileName), createGzip(), createWriteStream(archiveName));
+    }
+  });
 };
 
 await compress();
